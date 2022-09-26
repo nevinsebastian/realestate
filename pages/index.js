f8e27b4fd6b40b8804a5e1558ac2f3ecad12532d
nevinsebastian <nevinsebastian@cse.carmelcet.in>
2022-09-27 04:21:56 +0530

    .

--- pages/index.js
+++ pages/index.js
@@ -33,11 +33,12 @@ export default function Home({ propertyForSale, propertyForRent }) {
       linkName='/search?purpose=for-rent'
       imageUrl='https://bayut-production.s3.eu-central-1.amazonaws.com/image/145426814/33973352624c48628e41f2ec460faba4'
     />
-    <Flex flexWrap="wrap">
-      {propertyForRent.map((property)=> <Property property={property} key={property.id} />)}
 
-
+<Flex flexWrap='wrap'>
+      {propertyForRent.map((property) => <Property property={property} key={property.id} />)}
     </Flex>
+
+   
     <Banner
       purpose='BUY A HOME'
       title1=' Find, Buy & Own Your'
@@ -49,12 +50,10 @@ export default function Home({ propertyForSale, propertyForRent }) {
       imageUrl="https://bayut-production.s3.eu-central-1.amazonaws.com/image/110993385/6a070e8e1bae4f7d8c1429bc303d2008"
     />
    
-   <Flex flexWrap="wrap">
-      {propertyForSale.map((property)=> <Property property={property} key={property.id} />)}
-
-
+   <Flex flexWrap='wrap'>
+      {propertyForSale.map((property) => <Property property={property} key={property.id} />)}
     </Flex>
-
+  
   </Box>
   )
 }
